df94093970e1eb33f5b1e53c1f3741f827b88555
Ellie Ringwood <123974599+Ellie-Ringwood@users.noreply.github.com>
2025-02-13 17:25:16 +0000

    Whittled down unique situations to 82, started creating intentions for each possible action of every situation (190 intentions so far).
    Next: fix problem with general situation (fold when cant do any other option)

--- Situations.py
+++ Situations.py
@@ -1,19 +1,22 @@
+from abc import ABCMeta
 from DeckClass import Deck
 
 class SituationGenerator():
     def __init__(self):
         self.situations = []
-        self.possibleCards = Deck(1)
+        self.intentions = []        
+
+        self.possibleCards = Deck(1).cards
         self.rounds = 2
         #for card in self.possibleCards.cards:
         #    print(card)
         self.createSituations()
-        self.displaySituations()
-        self.findRepeats()
-        
+        self.displayArray(self.situations)
+        self.findRepeats(self.situations)
+        self.createIntentions()
+        self.findRepeats(self.intentions)
         
     def addSituation(self,roundNum, card, callCheckFund, canRaise, bluffBelief, communityCard):
-        #print(roundNum,card, callCheckFund, raiseFund, canRaise, bluffBelief, communityCard)
         match communityCard:
             case 0:
                 communityCardStatus = "same"
@@ -26,33 +29,20 @@ class SituationGenerator():
             bluffStatus = "none"
         else:
             bluffStatus = bool(bluffBelief)
+            
+        if(callCheckFund == False) and (canRaise == False):
+            situation = ["any","any", bool(callCheckFund), bool(canRaise), "null","null"]
+        else:
+            situation = [roundNum, card.getName(), bool(callCheckFund), bool(canRaise), bluffStatus, communityCardStatus]
+            #situation = [card.getName(), bool(callCheckFund), bool(canRaise), bluffStatus, communityCardStatus]
         
-        self.situations.append([roundNum, card, bool(callCheckFund), bool(canRaise), bluffStatus, communityCardStatus])
-        
-    """
+        if (situation not in self.situations):
+            self.situations.append(situation)
+
     def createSituations(self):
         for roundNum in range(self.rounds):
             for firstBetRound in range(0,2):
-                for card in range(len(self.possibleCards.cards)):
-                    for enoughForCallCheck in range(0,2):
-                        for enoughForRaise in range(0,2):
-                            for canRaise in range(0,2):
-                                if roundNum == 0: ## if first round, no community card
-                                    communityCard = -1
-                                    self.addSituation(card,enoughForCallCheck,enoughForRaise,canRaise,bluffBelief,-1)
-                                else:
-                                    communityCard = -1
-                                for communityCard in range(0,3):
-                                    if firstBetRound == 0: # first bet so can't guess bluff
-                                        self.addSituation(card,enoughForCallCheck,enoughForRaise,canRaise,-1,communityCard)
-                                    else:
-                                        for bluffBelief in range(0,2): #true or false, if agent believes opponent is bluffing 
-                                            self.addSituation(card,enoughForCallCheck,enoughForRaise,canRaise,bluffBelief,communityCard)
-    """
-    def createSituations(self):
-        for roundNum in range(self.rounds):
-            for firstBetRound in range(0,2):
-                for card in range(len(self.possibleCards.cards)):
+                for card in self.possibleCards:
                     for enoughForCallCheck in range(0,2):
                         for canRaise in range(0,2):
                             if roundNum == 0: ## if first round, no community card
@@ -69,29 +59,76 @@ class SituationGenerator():
                                         for bluffBelief in range(0,2): #true or false, if agent believes opponent is bluffing 
                                             self.addSituation(roundNum,card,enoughForCallCheck,canRaise,bluffBelief,communityCard)
          
+    def findRepeats(self, array):
+        newArray = []
+        for element in array:
+            if (element not in newArray):
+                newArray.append(element)
+        print(len(array)-len(newArray),"repeated elements")
 
-    
-    def findRepeats(self):
-        newSituations = []
+    def createIntentions(self):
+        profileScores = [0,0]
+        outcomeScore = 0
         for situation in self.situations:
-            if not(situation in newSituations):
-                newSituations.append(situation)
-        
-        print(len(newSituations))
+            possibleActions = self.getPossibleActions(situation)
+            for action in possibleActions:
+                #print(situation, action)
+                self.intentions.append([situation, profileScores, outcomeScore, action]) 
+        self.displayArray(self.intentions)
+            #self.intention.append() 
+            #print(situation[1:], self.getPossibleActions(situation))
+
+    def displayArray(self,array):
+        print(len(array))
         #print("Round, card, call/check, raise, bluff, community")
-        #for situation in self.situations:
-        #    print(situation)
+        for element in array:
+            print(element)
+            
 
+    def getPossibleActions(self, situation):
+        """
+        card = situation[0]
+        canCallCheck = situation[1]
+        canRaise = situation[2]
+        bluff = situation[3]
+        communityCard = situation[4]
+        """
+        roundNum = situation[0]
+        card = situation[1]
+        canCallCheck = situation[2]
+        canRaise = situation[3]
+        bluff = situation[4]
+        communityCard = situation[5]
+        
+        possibleActions = []
+        
+        if canRaise:
+            possibleActions.append("raise")
+        if canCallCheck:
+            possibleActions.append("call/check")
+        possibleActions.append("fold")
+        return possibleActions
+            
+    def findIntentions(self, roundNum, cardName, callCheckFund, canRaise, bluffBelief, communityCard):
+        print("FIND")
+        validIntentions = []
+        for intention in self.intentions:
+            situation = intention[0]
+            if (roundNum == situation[0])or(situation[0] == "any"):
+                if (cardName == situation[1])or(situation[1] == "any"):
+                    if (callCheckFund == situation[2]):
+                        if(canRaise == situation[3]):
+                            if(bluffBelief == situation[4]) or (situation[4] == "none"):
+                                if(communityCard == situation[5]) or (situation[5] == "none"):
+                                    print(intention)
+                                    #validIntentions.append(situation)
+        #for intention in validIntentions:
+        #    print(valid)
+    
 
+s = SituationGenerator()
 
-    def displaySituations(self):
-        print(len(self.situations))
-        print("Round, card, call/check, raise, bluff, community")
-        for situation in self.situations:
-            print(situation)
-
-
-SituationGenerator()
+s.findIntentions(0,'Queen',True,True,False,-1)
 
 
 
